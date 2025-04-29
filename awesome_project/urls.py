1de0e3b3093f71190497e910392da686541a60cb
monicaiyb <monicaiyb@gmail.com>
2025-04-29 13:54:40 +0300

    Add models for blog

--- awesome_project/urls.py
+++ awesome_project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views as blog
 
-import user
+
+
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    # path('', user.home, name='home'),
+    path('', blog.home, name='home'),
 ]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
-from django.db import models
-
+from django.db.models import Model
+from django.db.models import CharField, DateTimeField,BooleanField, ForeignKey,CASCADE
 # Create your models here.
+class Author(Model):
+    first_name=CharField(max_length=100)
+    last_name=CharField(max_length=100)
+    created_at=DateTimeField(auto_now_add=True)
+    is_published=BooleanField(default=False)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Blog (Model):
+    title=CharField(max_length=200)
+    description=CharField(max_length=500)
+    created_at=DateTimeField(auto_now_add=True)
+    completed=BooleanField(default=False)
+    artist = ForeignKey(Author, on_delete=CASCADE)
