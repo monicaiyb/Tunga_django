8382aaa9b04545b48fac890632e5c28eedf7e4c8
monicaiyb <monicaiyb@gmail.com>
2025-05-23 15:26:19 +0300

    Change view to class views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from blog import views
 
 urlpatterns = [
     path('blog/', views.blog_list),
     path('blog/<int:pk>/', views.blog_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- blog/views.py
+++ blog/views.py
@@ -1,50 +1,52 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from blog.models import Blog
 from blog.serializers import BlogSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-@csrf_exempt
-def blog_list(request):
+
+class BlogList(APIView):
     """
-    List all code snippets, or create a new snippet.
+    List all blog, or create a new snippet.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         blog = Blog.objects.all()
         serializer = BlogSerializer(blog, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BlogSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-@csrf_exempt
-def blog_detail(request, pk):
+class BlogDetail(APIView):
     """
-    Retrieve, update or delete a code blog.
+    Retrieve, update or delete a snippet instance.
     """
-    try:
-        blog = Blog.objects.get(pk=pk)
-    except Blog.DoesNotExist:
-        return HttpResponse(status=404)
+    def get_object(self, pk):
+        try:
+            return Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        blog = self.get_object(pk)
         serializer = BlogSerializer(blog)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = BlogSerializer(blog, data=data)
+    def put(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        serializer = BlogSerializer(blog, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        blog = self.get_object(pk)
         blog.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
